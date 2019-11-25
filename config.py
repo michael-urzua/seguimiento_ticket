2d4d3ccf011de37fc292d853265e18dd66033ab5
Michael/Urzua <michael-urzua>
2019-11-25 16:51:37 -0300

    cambio de conexion .117

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class conexion:
    @staticmethod
    def conect_post():
       try:
-         connection = psycopg2.connect(database = "central2010", user = "reporte_web", password = ".112233.", host = "10.20.12.100", port = "5432")
+         # connection = psycopg2.connect(database = "central2010", user = "reporte_web", password = ".112233.", host = "10.20.12.100", port = "5432")
+         connection = psycopg2.connect(database = "central2010", user = "postgres", password = "", host = "172.16.5.117", port = "5432")
          cursor=connection.cursor()
          return cursor
       except:
--- utils/globals.py
+++ utils/globals.py
@@ -1,4 +1,4 @@
 api="http://172.16.4.18:8082/api-auth/"#aldo
-url = "https://10.10.10.121:8082/api-auth/"
+url = "https://10.10.10.121:8082/api-auth-reporte/"
 url2 = "/base/checkPass/"
 url_chek = 'https://10.10.10.121:8082'
